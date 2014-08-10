dcdb9e585446b970bacf921c2bb67dd294b81dba
Jhon Vergara <jhonjvergarab@gmail.com>
2014-08-10 13:22:42 -0500

    Pantalla principal de la aplicación
    Pantalla principal de la aplicación

--- WS/A123Comun/gen/co/a123/a123comun/R.java
+++ WS/A123Comun/gen/co/a123/a123comun/R.java
@@ -1068,7 +1068,8 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int emergencia=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1102,6 +1103,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int llamarA123=0x7f05003e;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1126,7 +1128,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f05003f;
         public static final int title=0x7f05002b;
+        public static final int tomarFoto=0x7f05003c;
+        public static final int tomarVideo=0x7f05003d;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
